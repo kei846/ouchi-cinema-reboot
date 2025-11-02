11e0ac3a4a0c693d95700a711b20b436aad2b726
kei846 <camp.kei1997@gmail.com>
2025-11-02 15:20:12 +0900

    fix: use Image type from @sanity/types instead of SanityImageSource

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,6 +1,6 @@
 import { createClient } from 'next-sanity'
 import imageUrlBuilder from '@sanity/image-url'
-import type { SanityImageSource } from '@sanity/image-url'
+import type { Image } from '@sanity/types'
 
 const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
@@ -10,5 +10,5 @@ const client = createClient({
 })
 
 const builder = imageUrlBuilder(client)
-export const urlFor = (source: SanityImageSource) => builder.image(source)
+export const urlFor = (source: Image) => builder.image(source)
 export default client
