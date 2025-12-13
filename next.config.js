15d63b83c68cdc0c90f4d9b4696708b46803ae13
kei846 <camp.kei1997@gmail.com>
2025-12-13 10:23:02 +0900

    Feat: Allow Sanity image domain in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        port: '',
+        pathname: '/images/**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
